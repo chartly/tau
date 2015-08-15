be5dd6fc6f7acb76ed1a63ed3cdc285a68304f09
chartly <himself@thecorbinhart.com>
2015-08-15 07:38:54 -0700

    [src] Sketching out initial game state.

--- assets/src/ts/game.ts
+++ assets/src/ts/game.ts
@@ -4,12 +4,33 @@
 export class LevelScreen extends Phaser.State
 {
 	// members
-	bg : Phaser.Sprite;
-	playerShip : Phaser.Sprite;
+	bg : Phaser.TileSprite;
+	hero : Phaser.Sprite;
 	
 	// functions
 	constructor()
 	{
 		super();
 	}
+	
+	preload()
+	{
+		this.load.image("player-ship", "img/ckeegan/hero_1.png");
+		this.load.image("bg", "img/ckeegan/backdrop1.jpg");
+	}
+	
+	create()
+	{
+		this.physics.startSystem(Phaser.Physics.ARCADE);
+		
+		// set the background
+		this.bg = this.add.tileSprite(0, 0, this.scale.width, this.scale.height);
+		
+		// set the player up
+		this.hero = this.add.sprite(400, 500, "player-ship");
+		this.hero.anchor.setTo(.5, .5);
+		this.physics.enable(this.hero, Phaser.Physics.ARCADE);
+	}
+	
+	
 }
